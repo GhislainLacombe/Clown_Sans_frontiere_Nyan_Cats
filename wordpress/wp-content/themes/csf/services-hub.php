cd00a6c8fc8096ce1789bf3dd976848a51d6043f
Trish Nguyen <70410218+Trishng@users.noreply.github.com>
2021-11-26 16:23:01 -0500

    update

--- wordpress/wp-content/themes/csf/services-hub.php
+++ wordpress/wp-content/themes/csf/services-hub.php
@@ -11,21 +11,58 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
 	while ( have_posts() ) : the_post(); 
 ?>
 
-	<article>
 		<?php if (!is_front_page()) : // Si nous ne sommes PAS sur la page d'accueil ?>
-			<h2>
-				<?php the_title(); // Titre de la page ?>
-			</h2>
+			
 		<?php endif; ?>
 		
-		<?php the_content(); // Contenu principal de la page ?>
-	</article>
+		
+
+		<div class="hubServices">
+      
+      <h2 class="titre_hubServices">
+	  	<?php the_title(); // Titre de la page ?>
+	  </h2>
+      <div class="heroGenerique">
+
+      </div>
+      <h3 class="hubServices__description">
+	  	<?php the_content(); // Contenu principal de la page ?>
+      </h3>
+      
+      
+      <div class="cartesHubServices">
+       
+        <div class="card bg-dark text-white card-hubServices">
+          <img class="card-img card-img-hubServices" src="https://www.clownssansfrontieres.ca/wp-content/uploads/2014/04/CSF-milieu-scolaire-1024x413.jpg" alt="Card image">
+          <div class="hubServices-gradient"></div>
+          <div class="card-img-overlay card-img-overlay-hubServices">
+      
+            <h1 class="card-title-hubServices">Spectacles, animations et ateliers</h1>
+            
+          </div>
+        </div>
+        <div class="card bg-dark text-white card-hubServices">
+          
+          <img class="card-img card-img-hubServices" src="https://www.clownssansfrontieres.ca/wp-content/uploads/2017/09/Katel_Le_Fustec_20170711_133500-1024x683.jpg" alt="Card image">
+          <div class="hubServices-gradient"></div>
+          <div class="card-img-overlay card-img-overlay-hubServices">
+      
+            <h1 class="card-title-hubServices">Exposition <br>de photographies
+            </h1>
+            
+          </div>
+        </div>
+
+      </div>
+    </div>
+	
+
 <?php endwhile; // Fermeture de la boucle
 
 else : // Si aucune page n'a été trouvée
 	get_template_part( 'partials/404' ); // Affiche partials/404.php
 endif;
 
-get_sidebar(); // Affiche le contenu de sidebar.php
+//get_sidebar(); // Affiche le contenu de sidebar.php
 get_footer(); // Affiche footer.php 
 ?>
