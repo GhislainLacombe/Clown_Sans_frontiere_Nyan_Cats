89f2f11fe91135a8a961fb5cd44da7bea3207b25
Rebecca Pilotte <70409923+Bkkaaa@users.noreply.github.com>
2021-11-28 15:31:26 -0500

    boostrap links

--- wordpress/wp-content/themes/csf/footer.php
+++ wordpress/wp-content/themes/csf/footer.php
@@ -43,6 +43,9 @@
     </div>
   </div>
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+</script>
 <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
 <?php wp_footer(); 
 /* Espace où WordPress peut insérer des fichiers .js et autres. Par exemple pour des extensions (plugins). 
