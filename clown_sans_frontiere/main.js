9881cb8cd88133c0e63ed8ffd2d416a326d62234
Rebecca Pilotte <70409923+Bkkaaa@users.noreply.github.com>
2021-12-02 16:48:04 -0500

    last push for the day

--- clown_sans_frontiere/main.js
+++ clown_sans_frontiere/main.js
@@ -35,3 +35,11 @@ import './main.scss'
       el: '.swiper-pagination',
     },
   });
+
+  // const hamburger = document.querySelector('.domino'); //element 
+
+  // const hover = gsap.to('.domino', //animation
+  //   { repeat: 2, y:10, yoyo:true, paused:true});
+  
+  // hamburger.addEventListener('mouseenter', ()=> hover.play());
+  // hamburger.addEventListener('mouseleave', ()=> hover.reverse());
