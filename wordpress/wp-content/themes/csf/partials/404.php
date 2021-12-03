d7be52779e6b4402e2fa28a442c3ed986d192243
Trish Nguyen <70410218+Trishng@users.noreply.github.com>
2021-12-03 11:33:40 -0500

    gsap footer

--- wordpress/wp-content/themes/csf/partials/404.php
+++ wordpress/wp-content/themes/csf/partials/404.php
@@ -1,17 +1,15 @@
-<section>
   <div class="error_wrapper">
-    <div class="error__txt"><h1>ERREUR</h1></div>
-    <div class="error">
-      <div class="ball__wrapper">
-        <div class="ball ball__one"><h3>0</h3></div>
-        <div class="ball ball__two"><h3>4</h3></div>
-        <div class="ball ball__three"><h3>4</h3></div>
-        <div class="ball ball__four"><h3>• ᴗ •</h3></div>
-      </div>
-    </div>
-    <div class="error__image">
-        <img class="clown__arms" src="https://i.imgur.com/vesHHhb.png?1" alt="arms">
-      <img class="clown__hair" src="https://i.imgur.com/7y7zDHz.png?1" alt="hair">
+  <div class="error__txt"><h1>ERREUR</h1></div>
+  <div class="error">
+    <div class="ball__wrapper">
+      <div class="ball ball__one"><h3>0</h3></div>
+      <div class="ball ball__two"><h3>4</h3></div>
+      <div class="ball ball__three"><h3>4</h3></div>
+      <div class="ball ball__four"><h3>•ᴗ•</h3></div>
     </div>
   </div>
-</section>
+  <div class="error__image">
+      <img class="clown__arms" src="https://i.imgur.com/vesHHhb.png?1" alt="arms">
+    <img class="clown__hair" src="https://i.imgur.com/7y7zDHz.png?1" alt="hair">
+  </div>
+</div>
