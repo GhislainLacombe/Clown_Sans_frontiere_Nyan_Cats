ae6867fcaa3070a215258ed8ad6d15f1ed7886a3
Trish Nguyen <70410218+Trishng@users.noreply.github.com>
2021-12-03 11:06:33 -0500

    404 sur vite

--- wordpress/wp-content/themes/csf/partials/404.php
+++ wordpress/wp-content/themes/csf/partials/404.php
@@ -1,4 +1,17 @@
 <section>
-  <h2>404</h2>
-	<img src="https://media.giphy.com/media/3HLMNi9U0yb4CDldVO/giphy.gif" alt="404">
+  <div class="error_wrapper">
+    <div class="error__txt"><h1>ERREUR</h1></div>
+    <div class="error">
+      <div class="ball__wrapper">
+        <div class="ball ball__one"><h3>0</h3></div>
+        <div class="ball ball__two"><h3>4</h3></div>
+        <div class="ball ball__three"><h3>4</h3></div>
+        <div class="ball ball__four"><h3>• ᴗ •</h3></div>
+      </div>
+    </div>
+    <div class="error__image">
+        <img class="clown__arms" src="https://i.imgur.com/vesHHhb.png?1" alt="arms">
+      <img class="clown__hair" src="https://i.imgur.com/7y7zDHz.png?1" alt="hair">
+    </div>
+  </div>
 </section>
